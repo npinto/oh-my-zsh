6a07b242f86ac92bad2e185985b2e546f0652d7c
Nicolas Pinto <pinto@alum.mit.edu>
2012-04-11 13:08:58 -0400

    BUG: fix identation problem

--- plugins/git-prompt/gitstatus.py
+++ plugins/git-prompt/gitstatus.py
@@ -2,11 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 # change those symbols to whatever you prefer
-symbols = {'ahead of': '↑', 'behind': '↓', 'staged':'♦', 'changed':'‣', 'untracked':'…', 'clean':'⚡', 'unmerged':'≠', 'sha1':':'}
+symbols = {'ahead of': '↑', 'behind': '↓', 'staged': '♦', 'changed': '‣', 'untracked': '…', 'clean': '⚡', 'unmerged': '≠', 'sha1': ': '}
 
 from subprocess import Popen, PIPE
 
-output,error = Popen(['git','status'], stdout=PIPE, stderr=PIPE).communicate()
+output, error = Popen(['git', 'status'], stdout=PIPE, stderr=PIPE).communicate()
 
 if error:
 	import sys
@@ -23,6 +23,7 @@ changed = re.compile(r'^# Changed but not updated:$', re.MULTILINE)
 untracked = re.compile(r'^# Untracked files:$', re.MULTILINE)
 unmerged = re.compile(r'^# Unmerged paths:$', re.MULTILINE)
 
+
 def execute(*command):
 	out, err = Popen(stdout=PIPE, stderr=PIPE, *command).communicate()
 	if not err:
@@ -32,13 +33,13 @@ def execute(*command):
 	return nb
 
 if staged.search(output):
-	nb = execute(['git','diff','--staged','--name-only','--diff-filter=ACDMRT'])
+	nb = execute(['git', 'diff', '--staged', '--name-only', '--diff-filter=ACDMRT'])
 	status += '%s%s' % (symbols['staged'], nb)
 if unmerged.search(output):
-	nb = execute(['git','diff', '--staged','--name-only', '--diff-filter=U'])
+	nb = execute(['git', 'diff', '--staged', '--name-only', '--diff-filter=U'])
 	status += '%s%s' % (symbols['unmerged'], nb)
 if changed.search(output):
-	nb = execute(['git','diff','--name-only', '--diff-filter=ACDMRT'])
+	nb = execute(['git', 'diff', '--name-only', '--diff-filter=ACDMRT'])
 	status += '%s%s' % (symbols['changed'], nb)
 if untracked.search(output):
 ## 		nb = len(Popen(['git','ls-files','--others','--exclude-standard'],stdout=PIPE).communicate()[0].splitlines())
@@ -51,7 +52,7 @@ remote = ''
 
 bline = lines[0]
 if bline.find('Not currently on any branch') != -1:
-	branch = symbols['sha1']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0][:-1]
+	branch = symbols['sha1'] + Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE).communicate()[0][:-1]
 else:
 	branch = bline.split(' ')[3]
 	bstatusline = lines[1]
@@ -61,8 +62,7 @@ else:
 		remote += match.groups()[2]
 	elif lines[2:]:
 		div_match = diverge_re.match(lines[2])
-	 	if div_match:
+		if div_match:
 			remote = "{behind}{1}{ahead of}{0}".format(*div_match.groups(), **symbols)
 
-print '\n'.join([branch,remote,status])
-
+print '\n'.join([branch, remote, status])
